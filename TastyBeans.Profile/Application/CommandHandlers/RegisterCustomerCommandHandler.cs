1ab933917e400a8dfd49cb21224435f543becd79
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-09-28 10:31:47 +0200

    Add skeleton

---
+++ TastyBeans.Profile/Application/CommandHandlers/RegisterCustomerCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Marten;
+
+using TastyBeans.Profile.Domain.Aggregates.CustomerAggregate;
+using TastyBeans.Profile.Domain.Aggregates.CustomerAggregate.Commands;
+
+namespace TastyBeans.Profile.Application.CommandHandlers;
+
+public class RegisterCustomerCommandHandler
+{
+    private readonly IDocumentSession _documentSession;
+
+    public RegisterCustomerCommandHandler(IDocumentSession documentSession)
+    {
+        _documentSession = documentSession;
+    }
+
+    public async Task HandleAsync(RegisterCustomer cmd)
+    {
+        
+    }
+}
--- TastyBeans.Profile/Program.cs
+++ TastyBeans.Profile/Program.cs
@@ -1,6 +1,24 @@
+using Marten;
+
+using TastyBeans.Profile.Application.CommandHandlers;
+using TastyBeans.Profile.Domain.Aggregates.CustomerAggregate.Commands;
+
+using Weasel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMarten(marten =>
+{
+    marten.Connection(builder.Configuration.GetConnectionString("DefaultDatabase"));
+    marten.AutoCreateSchemaObjects = AutoCreate.CreateOrUpdate;
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/customers", async (RegisterCustomer cmd, RegisterCustomerCommandHandler handler) =>
+{
+    await handler.HandleAsync(cmd);
+    return Results.Accepted();
+});
 
 app.Run();
